a5ae1a572a8a0c000fda5ffc0d4b651955c85a16
tw1997 <t888y@qq.com>
2017-08-22 09:44:11 +0800

    添加eslint

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,24 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+function resolve (dir) {
+    return path.join(__dirname, dir);
+}
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.(js|vue)$/,
+                enforce: 'pre',
+                include: resolve('resources/assets'),
+                use: [{
+                    loader: 'eslint-loader',
+                    options: {
+                        formatter: require('eslint-friendly-formatter')
+                    }
+                }]
+            }
+        ]
+    }
+});
+mix.js('resources/assets/admin/app.js', 'public/js/admin')
+    .version();
