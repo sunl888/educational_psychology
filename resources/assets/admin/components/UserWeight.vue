62e242f4c1adc148846391529dc9f11cbea83133
tw1997 <t888y@qq.com>
2017-08-23 16:38:03 +0800

    添加用户组件

---
+++ resources/assets/admin/components/UserWeight.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="m-info">
+    <a href="#" target="_blank">
+      <i class="user-pic" style="background-image: url(http://static.hdslb.com/images/member/noface.gif)"></i>
+      <span class="user-name">ty6666</span>
+    </a>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped lang="less">
+  .m-info {
+    padding-left: 30px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    width: 200px;
+    .user-pic {
+      display: inline-block;
+      width: 30px;
+      height: 30px;
+      border-radius: 100%;
+      vertical-align: middle;
+      background-size: 100% 100%;
+      margin-right: 8px;
+    }
+    .user-name {
+      color: #222;
+    }
+  }
+</style>
