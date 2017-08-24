b962db5b6739c18014fef464c9e3b1c849a45f40
tw1997 <t888y@qq.com>
2017-08-24 20:24:24 +0800

    用户名字段修改
    字段改为account 可输入用户名或者邮箱

---
+++ resources/assets/backend/views/Login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="login">
+      <main class="login_wrapper">
+        <h2>用户登录</h2>
+         <Form :model="loginInfo">
+            <Form-item>
+              <Input v-model="loginInfo.account" placeholder="请输入用户名或邮箱"></Input>
+            </Form-item>
+            <Form-item>
+              <Input v-model="loginInfo.password" type="password" placeholder="请输入密码" @keydown.enter="login"></Input>
+            </Form-item>
+            <Form-item>
+              <Button :loading="loading" class="login_btn" type="primary" long @click="login">确认提交</Button>
+              <Checkbox v-model="loginInfo.remeber">下次自动登录</Checkbox>
+            </Form-item>
+        </Form>
+        <Button class="back_btn" type="text"><Icon class="icon" type="arrow-left-c"></Icon>返回到首页</Button>
+      </main>
+  </div>  
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false,
+      loginInfo: {
+        account: '',
+        password: '',
+        remeber: true
+      }
+    };
+  },
+  methods: {
+    login () {
+      this.loading = true;
+    }
+  }
+};
+</script>
+
+
+<style lang="less" scoped>
+.login{
+  padding-top: 120px;
+  .login_wrapper{
+    position: relative;
+    h2{
+      font-size: 26px;
+      text-align: center;
+      font-weight: 700;
+      color: #506470;
+      margin: 10px 0 30px;
+    }
+    width: 350px;
+    padding: 24px;
+    margin: 72px auto 68px;
+    background: #fff;
+    box-shadow: 0 1px 2px rgba(0,0,0,.2);
+    .login_btn{
+      margin-bottom: 5px;
+    }
+    .back_btn{
+      position: absolute;
+      bottom: -70px;
+      .icon{
+        margin-right: 5px;
+      }
+    }
+  }
+}
+</style>
+
