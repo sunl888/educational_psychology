afd276168b503aae99a8951c49399c455e6af801
tw1997 <t888y@qq.com>
2017-08-29 02:01:27 +0800

    添加路由切换进度条

--- resources/assets/backend/app.js
+++ resources/assets/backend/app.js
@@ -4,9 +4,17 @@ import router from './router';
 import App from './App.vue';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start();
+  next();
+});
+router.afterEach((to, from, next) => {
+  iView.LoadingBar.finish();
+});
+
 Vue.use(iView);
 Vue.use(thttp, {
-  baseURL: '/api/backend'
+  baseURL: '/api/backend/'
 });
 new Vue({
   el: '#app',
