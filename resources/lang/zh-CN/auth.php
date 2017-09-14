489faf138c96365e6d6316452cddaf3137219aad
taoyu <1255803549@qq.com>
2017-09-15 02:30:13 +0800

    update

--- resources/lang/zh-CN/auth.php
+++ resources/lang/zh-CN/auth.php
@@ -15,5 +15,7 @@ return [
 
     'failed'   => '用户名或密码错误。',
     'throttle' => '您的尝试登录次数过多，请 :seconds 秒后再试。',
-
+    'user_locked' => '该用户已经被锁定',
+    'password_error' => '密码错误',
+    'please_login_first' => '请先登录！'
 ];
