ca48540a60fd44c26e476d65a7be889ba7c6de23
taoyu <1255803549@qq.com>
2017-09-21 23:38:19 +0800

    修改路由名称

--- routes/frontend/web.php
+++ routes/frontend/web.php
@@ -4,7 +4,7 @@ Route::middleware('record_visitor')->group(function (){
 
     Route::get('/', 'IndexController@index')->name('index');
 
-    Route::get('/categories/{slug}', 'CategoriesController@show')->name('showCategory');
+    Route::get('/categories/{slug}', 'CategoriesController@show')->name('category.show');
 
-    Route::get('/posts/{slug}', 'PostsController@show')->name('showPost');
+    Route::get('/posts/{slug}', 'PostsController@show')->name('post.show');
 });
