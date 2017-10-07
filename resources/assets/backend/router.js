0eeb9a0647c9fae173c4f4007fc781a487293a87
taowei <t888y@qq.com>
2017-10-07 16:17:10 +0800

    fixed bug

--- resources/assets/backend/router.js
+++ resources/assets/backend/router.js
@@ -201,8 +201,8 @@ router.afterEach((to, from) => {
     } catch (e) {
       document.title = 'tiny';
     }
-  }, 0)
-})
+  }, 0);
+});
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!localStorage.getItem('login_ok')) {
