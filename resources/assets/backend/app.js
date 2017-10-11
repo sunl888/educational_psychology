a473747fa0b34edd534a5f4c1ac8dc15fdedc698
taowei <t888y@qq.com>
2017-10-11 20:52:35 +0800

    fixed bug

--- resources/assets/backend/app.js
+++ resources/assets/backend/app.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import tHttp from '../utils/tHttp';
+import tHttp from './utils/tHttp';
 import router from './router';
 import App from './App.vue';
 import iView from 'iview';
