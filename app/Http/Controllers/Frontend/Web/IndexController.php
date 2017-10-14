5d183d56a6296236d416fe0337581911e747e955
taoyu <1255803549@qq.com>
2017-10-14 18:43:03 +0800

    image to qiniu

--- app/Http/Controllers/Frontend/Web/IndexController.php
+++ app/Http/Controllers/Frontend/Web/IndexController.php
@@ -9,6 +9,7 @@ class IndexController extends Controller
 {
     public function index()
     {
+
         return view(config('template.theme_namespace') . '::index');
     }
 }
--- config/ueditor.php
+++ config/ueditor.php
@@ -11,7 +11,7 @@
 
 return [
     // 存储引擎: config/filesystem.php 中 disks， public 或 qiniu
-    'disk' => 'public',
+    'disk' => 'qiniu',
     'route' => [
         'name' => '/ueditor/server',
         'options' => [
