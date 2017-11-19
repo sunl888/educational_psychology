cc2efac301f34712a5d9c06d0b2ed5a026673845
taoyu <1255803549@qq.com>
2017-11-20 00:10:12 +0800

    使用 apiResource 方法

--- routes/backend/api.php
+++ routes/backend/api.php
@@ -7,23 +7,17 @@ Route::post('auth/logout', 'Auth\LoginController@logout');
 Route::post('ajax_upload_image', 'ImageController@upload');
 Route::post('wang_editor_upload_image', 'ImageController@wangEditorUpload');
 
-Route::resource('users', 'UsersController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('users', 'UsersController');
 
 Route::get('me', 'UsersController@me');
 
 // 获取所有角色(不分页 用于添加用户时显示)
 Route::get('roles/all', 'RolesController@allRoles');
-Route::resource('roles', 'RolesController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('roles', 'RolesController');
 
 Route::get('roles/{role}/permissions', 'RolesController@permissions');
 
-Route::resource('posts', 'PostsController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('posts', 'PostsController');
 // 真删除指定的文章
 Route::delete('posts/{post}/real', 'PostsController@realDestroy');
 // 恢复指定的被软删除的文章
@@ -32,40 +26,26 @@ Route::post('posts/{post}/restore', 'PostsController@restore');
 Route::get('templates', 'TemplatesController@templates');
 
 Route::get('categories/visual_output', 'CategoriesController@visualOutput');
-Route::resource('categories', 'CategoriesController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('categories', 'CategoriesController');
 
 // 获取单页
 Route::get('categories/{category}/page', 'CategoriesController@page');
 // 创建或更新单页
 Route::post('categories/{category}/page', 'CategoriesController@savePage');
-Route::resource('banners', 'BannersController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('banners', 'BannersController');
 
-Route::resource('links', 'LinksController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('links', 'LinksController');
 
 Route::post('settings/index_order', 'SettingsController@setOrder');
-Route::resource('settings', 'SettingsController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('settings', 'SettingsController');
 
 
-Route::resource('types', 'TypesController', [
-    'except' => ['create', 'edit']
-]);
+Route::apiResource('types', 'TypesController');
 
 // 获取统计数据
 Route::get('statistics', 'HomeController@index');
 
 
-Route::resource('attachments', 'AttachmentsController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('attachments', 'AttachmentsController');
 
-Route::resource('tags', 'TagsController', [
-    'except'=> ['create', 'edit']
-]);
+Route::apiResource('tags', 'TagsController');
