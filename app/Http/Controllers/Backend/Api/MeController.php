261e6ce09f7a0107e84d17394a4d704038bd6c63
taoyu <1255803549@qq.com>
2017-11-22 16:28:13 +0800

    后台管理员当前登录的信息

---
+++ app/Http/Controllers/Backend/Api/MeController.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Controllers\Backend\Api;
+
+
+use App\Http\Controllers\ApiController;
+use App\Transformers\Backend\UserTransformer;
+use Auth;
+
+class MeController extends ApiController
+{
+    public function show()
+    {
+        return $this->response()->item(Auth::user(), new UserTransformer());
+    }
+}
