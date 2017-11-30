7f8ca58f682c267ae6b051c10117c214f86c4426
taoyu <1255803549@qq.com>
2017-12-01 00:35:51 +0800

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\PostContent;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,20 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        factory(\App\Models\Category::class, 5)->create()->each(function ($category) {
-//            factory(\App\Models\Category::class, random_int(0, 3))->create(['parent_id' => $category->id]);
-//        });
-//
-//        factory(App\Models\User::class, 10)->create()->each(function ($user) {
-//            factory(App\Models\Post::class, random_int(0, 10))->create(['user_id' => $user->id])->each(function ($post) {
-//                $post->postContent()->save(factory(PostContent::class)->make());
-//            });
-//        });
-//
-//        DB::table('users')->where('id', 1)->update(['user_name' => 'tiny', 'email' => 'tiny@test.com', 'locked_at' => null]);
-//
+        factory(\App\Models\Category::class, 5)->create()->each(function ($category) {
+            factory(\App\Models\Category::class, random_int(0, 3))->create(['parent_id' => $category->id]);
+        });
+
+        factory(App\Models\User::class, 10)->create()->each(function ($user) {
+            factory(App\Models\Post::class, random_int(0, 10))->create(['user_id' => $user->id])->each(function ($post) {
+                $post->postContent()->save(factory(PostContent::class)->make());
+            });
+        });
+
         $this->call(PermissionsTableSeeder::class);
-//        $this->call(RolesTableSeeder::class);
-//        $this->call(TypesTableSeeder::class);
+        $this->call(TypesTableSeeder::class);
+        User::find(1)->assignRole('admin')->update(['user_name' => 'tiny', 'email' => 'tiny@test.com', 'locked_at' => null]);
+
+
+        // $this->call(RolesTableSeeder::class);
+
     }
 }
