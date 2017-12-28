79f80fd5fd36f8f7587c1c57e03d3c6316558aa6
taoyu <1255803549@qq.com>
2017-12-28 22:13:15 +0800

    fixed bug

--- app/Http/Requests/Backend/TypeRequest.php
+++ app/Http/Requests/Backend/TypeRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Backend;
 
 use App\Http\Requests\Request;
 use App\Models\Type;
+use Illuminate\Validation\Rule;
 
 class TypeRequest extends Request
 {
@@ -25,6 +26,11 @@ class TypeRequest extends Request
      */
     public function rules()
     {
+        $uniqueRule = Rule::unique('types');
+        $modelName = $this->get('model_name');
+        if (in_array($modelName, Type::$modelMapWithType, true))
+            $uniqueRule->where('model_name', $modelName);
+
         switch ($this->method()) {
             case 'GET':
             case 'DELETE':
@@ -32,7 +38,7 @@ class TypeRequest extends Request
             case 'POST':
                 return [
                     // todo name 和 model_name 的组合索引
-                    'name' => ['required', 'alpha_dash', 'between:1,30'],
+                    'name' => ['required', 'alpha_dash', 'between:1,30', $uniqueRule],
                     'display_name' => ['required', 'string', 'between:1,30'],
                     'description' => ['nullable', 'string', 'between:2,190'],
                     // model_name 指定 Model 类名 表示是该 Model 的类别, 这里只需要传入对应的别名，别名参考 Type::$modelMapWithType
@@ -40,8 +46,9 @@ class TypeRequest extends Request
                 ];
             case 'PUT':
             case 'PATCH':
+                $type = $this->route('type');
                 return [
-                    'name' => ['nullable', 'alpha_dash', 'between:1,30'],
+                    'name' => ['nullable', 'alpha_dash', 'between:1,30', $uniqueRule->ignore($type->id)],
                     'display_name' => ['nullable', 'string', 'between:1,30'],
                     'description' => ['nullable', 'string', 'between:2,190'],
                 ];
