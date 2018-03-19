b250725be9c869fa5f9080c6e476504461c6e16a
taoyu <1255803549@qq.com>
2018-03-20 02:20:58 +0800

    切换编辑器

--- resources/views/vendor/ueditor/assets.blade.php
+++ resources/views/vendor/ueditor/assets.blade.php
@@ -1,7 +1,7 @@
 <!-- 配置文件 -->
-<script type="text/javascript" src="{{ cdn('vendor/ueditor/ueditor.config.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/neditor/neditor.config.js') }}"></script>
 <!-- 编辑器源码文件 -->
-<script type="text/javascript" src="{{ cdn('vendor/ueditor/ueditor.all.js') }}"></script>
+<script type="text/javascript" src="{{ asset('vendor/neditor/neditor.all.js') }}"></script>
 <script>
     window.UEDITOR_CONFIG.serverUrl = '{{ route(config('ueditor.route.name')) }}'
 </script>
